95f467b52583715607c03c0a09afd4c47dae0004
ivan <iva1989@icloud.com>
2020-10-30 10:14:30 +0500

    create own framework and add dependency

---
+++ Modules/DBHelper/DBHelper/DBHelper.h
@@ -0,0 +1,18 @@
+//
+//  DBHelper.h
+//  DBHelper
+//
+//  Created by admin on 30.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DBHelper.
+FOUNDATION_EXPORT double DBHelperVersionNumber;
+
+//! Project version string for DBHelper.
+FOUNDATION_EXPORT const unsigned char DBHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBHelper/PublicHeader.h>
+
+
